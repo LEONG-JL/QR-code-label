d5651c8afb3edce217b684b23b5401ef6af0a823
LEONG-JL <jinliangy8@gmail.com>
2025-05-15 14:31:17 +0800

    changes to line 43-47

--- app.py
+++ app.py
@@ -42,7 +42,10 @@ def lmt():
 
         # Load Excel database
         df = pd.read_excel(EXCEL_DB_PATH)
+        df['RESMED GTIN'] = pd.to_numeric(df['RESMED GTIN'], errors='coerce')
+        df.dropna(subset=['RESMED GTIN'], inplace=True)
         df['RESMED GTIN'] = df['RESMED GTIN'].astype(int)
+
         row = df[df['RESMED GTIN'] == gtin_from_udi]
 
         if row.empty:
